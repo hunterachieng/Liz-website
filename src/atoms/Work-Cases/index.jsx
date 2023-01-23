2b6d4d8a0fac7178e3ee4a53ba0f20744d72c6b9
Hunter <synthia@ipayafrica.com>
2023-01-23 11:21:36 +0300

    change color

--- src/atoms/Work-Cases/index.jsx
+++ src/atoms/Work-Cases/index.jsx
@@ -7,6 +7,7 @@ const WorkCases = ({imageUrl,heading,content, bgColor})=>{
         <div className={`w-7/12 text-white  flex flex-col text-left my-10 mx-24 sm:w-10/12 sm:mx-4 xsm:w-10/12 xsm:mx-4 md:w-10/12 md:mx-4`}>
             <h1 className="font-bold" >{heading}</h1>
             <br/>
+            
             <p >{content}</p>
             <img src={imageUrl} alt="Case study images" className="mt-4 ml-2"/>
             <div className="flex space-x-2 mt-2">
