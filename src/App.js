dcef7a5908af068ce5fa812653b14e6d4b3f6d2d
Hunter <synthia@ipayafrica.com>
2023-01-23 11:01:32 +0300

    complete homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import Work from './components/Work';
+import About from './components/About';
+import Contacts from './components/Contacts';
+import Resume from './components/Resume';
 import Layout from './components/Layout';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
-    <div >
-      <Layout/>
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' exact element={<Work/>}/>
+        <Route path='/about' exact element={<About/>}/>
+        <Route path='/resume' exact element={<Resume/>}/>
+        <Route path='/contacts' exact element={<Contacts/>}/>
+
+      </Routes>
+     <Footer/>
+    </Router>
   );
 }
 
